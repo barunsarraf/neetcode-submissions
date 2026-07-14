539c5bf92680428502ebb3d73d8cc3874138623b
Barun Sarraf <barunsaraf1@gmail.com>
2026-07-15 01:33:10 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ""
+        for c in s:
+            if c.isalpha() or c.isnumeric():
+                new_s+=c
+        return new_s.lower()==new_s[::-1].lower()
